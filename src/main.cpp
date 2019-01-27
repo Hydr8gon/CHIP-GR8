a5e58ef577353ed4e3ba65c5474c957ac7d6c700
Sean Maas <seanmaas27@gmail.com>
2019-01-27 17:22:01 -0500

    fix FX33, FX55 and FX65 for real this time. i need to learn how to read

--- src/main.cpp
+++ src/main.cpp
@@ -287,19 +287,19 @@ void run_cycle() {
                     break;
 
                 case 0x0033: // FX33: Stores the binary-coded decimal representation of VX at VI
-                    data_registers[address_register    ] = (data_registers[(opcode & 0x0F00) >> 8] / 100);
-                    data_registers[address_register + 1] = (data_registers[(opcode & 0x0F00) >> 8] / 10) % 10;
-                    data_registers[address_register + 2] = (data_registers[(opcode & 0x0F00) >> 8] % 100) % 10;
+                    memory[address_register    ] = (data_registers[(opcode & 0x0F00) >> 8] / 100);
+                    memory[address_register + 1] = (data_registers[(opcode & 0x0F00) >> 8] / 10) % 10;
+                    memory[address_register + 2] = (data_registers[(opcode & 0x0F00) >> 8] % 100) % 10;
                     program_offset += 2;
                     break;
 
                 case 0x0055: // FX55: Stores V0 to VX in memory starting at VI
-                    memcpy(&data_registers[address_register], data_registers, ((opcode & 0x0F00) >> 8) + 1);
+                    memcpy(&memory[address_register], data_registers, ((opcode & 0x0F00) >> 8) + 1);
                     program_offset += 2;
                     break;
 
                 case 0x0065: // FX65: Fills V0 to VX in memory with values starting at VI
-                    memcpy(data_registers, &data_registers[address_register], ((opcode & 0x0F00) >> 8) + 1);
+                    memcpy(data_registers, &memory[address_register], ((opcode & 0x0F00) >> 8) + 1);
                     program_offset += 2;
                     break;
 
